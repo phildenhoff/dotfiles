4bac4c14c59e1dda571a02c40a1a53c304387e07
Phil Denhoff <phil@denhoff.ca>
2024-05-23 10:03:04 -0700

    feat[wezterm]: Make Alt+Left and Alt+Right work again

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,3 +1,5 @@
+local wezterm = require 'wezterm';
+
 return {
   color_scheme = "Catppuccin Latte",
   enable_tab_bar = false,
@@ -5,4 +7,10 @@ return {
   window_background_opacity = 1,
   window_decorations = "RESIZE",
   front_end = "WebGpu"
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}}
+  }
 }
