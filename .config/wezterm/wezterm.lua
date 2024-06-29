77749ea7d259763dfda1ffb785573976d4eba287
Phil Denhoff <phil@denhoff.ca>
2024-06-29 11:31:08 -0700

    feat[wezterm]: Fix fonts rendering as coloured squares instead of text

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,5 +3,6 @@ return {
   enable_tab_bar = false,
   use_fancy_tab_bar = false,
   window_background_opacity = 1,
-  -- window_decorations = "RESIZE"
+  window_decorations = "RESIZE",
+  front_end = "WebGpu"
 }
