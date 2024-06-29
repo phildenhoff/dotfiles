c6df124315b5ddb1a52d2c3f5650ef66123d6315
Phil Denhoff <phil@denhoff.ca>
2024-06-29 10:10:03 -0700

    feat(knife)[ssh]: enable SSH

--- knife/etc/nixos/configuration.nix
+++ knife/etc/nixos/configuration.nix
@@ -102,7 +102,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
