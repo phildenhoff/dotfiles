6946b1b9a024363b3b9c16ad2648010624c14d35
Phil Denhoff <phil@denhoff.ca>
2024-06-29 11:31:08 -0700

    feat[wezterm]: Correct config syntax

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
   use_fancy_tab_bar = false,
   window_background_opacity = 1,
   window_decorations = "RESIZE",
-  front_end = "WebGpu"
+  front_end = "WebGpu",
   keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
