9f1a64983f1c3c4784425e75408f091ba39b2f6f
Phil Denhoff <phil@denhoff.ca>
2024-06-30 18:04:32 -0700

    feat(knife): swap gnome with plasma6

--- knife/etc/nixos/configuration.nix
+++ knife/etc/nixos/configuration.nix
@@ -24,9 +24,9 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   # Configure keymap in X11
   #services.xserver = {
