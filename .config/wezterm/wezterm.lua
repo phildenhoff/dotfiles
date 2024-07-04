bcdad3fc62ca85f4d47bf48f09103216621298c2
Phil Denhoff <phil@denhoff.ca>
2024-07-04 10:51:11 -0700

    feat[wezterm]: opaque window

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 return {
   color_scheme = "Catppuccin Latte",
   enable_tab_bar = false,
-  -- use_fancy_tab_bar = false,
-  window_background_opacity = 0.8,
+  use_fancy_tab_bar = false,
+  window_background_opacity = 1,
   -- window_decorations = "RESIZE"
 }
